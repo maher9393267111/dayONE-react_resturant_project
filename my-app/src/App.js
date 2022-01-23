4c30545216a50f7d4423789aabc2d04c3669b98f
maher <gomemahero@gmail.com>
2022-01-23 14:19:35 +0300

    push every thing

---
+++ my-app/src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+// import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom"
+
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer";
+
+import Home from "./pages/Home";
+import Recipes from "./pages/Recipes";
+import Settings from "./pages/Settings";
+
+
+
+
+function App() {
+  return (
+    <Router>
+    <Navbar />
+    <div className="container main">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/recipes" element={<Recipes />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
+    </div>
+    <Footer />
+  </Router>
+  );
+}
+
+export default App;
